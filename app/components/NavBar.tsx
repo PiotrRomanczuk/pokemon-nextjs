5fd230d59c2e74574187218d7cf5a8c7da9c5480
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-02 00:50:54 +0200

    Init repo

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+const NavBar = () => {
+  return (
+      <nav className="min-w-max h-10 bg-slate-400 flex gap-5 flex-row-reverse
+      ">
+          <h1 className="my-auto mr-4">Hello</h1>
+          <h1 className="my-auto">Hello</h1>
+          <h1 className="my-auto">Hello</h1>
+          <h1 className="my-auto">Hello</h1>
+      </nav>
+  )
+}
+
+export default NavBar
