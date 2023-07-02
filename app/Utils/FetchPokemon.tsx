ad0891e0c3adfd0789fb6763f39f83e6ddf2ccbe
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-02 19:58:27 +0200

    Preparing to pass the props from Fetching Poke to the Card

--- app/Utils/FetchPokemon.tsx
+++ app/Utils/FetchPokemon.tsx
@@ -1,32 +1,51 @@
 import React, { useState } from 'react';
+import RandomNumber from './RandomNumber';
+
+export type Pokemon = {
+	name: string;
+	image: string;
+	id: number;
+	// HP: number;
+	// Attack: number;
+	// Defense: number;
+};
+
+const mainURL = 'https://pokeapi.co/api/v2';
+
+export const fetchPoke = async (): Promise<Pokemon | null> => {
+	let pokeAmount = RandomNumber(151);
+
+	try {
+		const response = await fetch(`${mainURL}/pokemon/${pokeAmount}`);
+		const data = await response.json();
+
+		const fetchedPoke: Pokemon = {
+			name: data.name,
+			id: data.id,
+			image: data.sprites.front_default,
+		};
+
+		console.log(fetchedPoke);
+
+		return fetchedPoke;
+	} catch (error) {
+		console.log(`Error: ${error}`);
+		return null;
+	}
+};
 
 export default function FetchPokemon() {
+	const [fetchedPoke, setFetchedPoke] = useState<Pokemon | null>(null);
 
-    type Pokemon = {
-        name: string,
-        image: string,
-        id: number,
-    };
+	const handleFetch = async () => {
+		const fetchedData = await fetchPoke();
+		setFetchedPoke(fetchedData);
+	};
 
-    const mainURL = 'https://pokeapi.co/api/v2';
-    const [fetchedPoke, setFetchedPoke] = useState<Pokemon | null>(null);
-
-    const fetchPoke = async () => {
-        try {
-            const response = await fetch(`${mainURL}/pokemon/1`);
-            const data = await response.json();
-
-            const fetchedPoke: Pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites.front_default,
-            };
-            console.log(fetchedPoke);
-            console.log(fetchedPoke.image);
-
-            setFetchedPoke(fetchedPoke);
-        } catch (error) {
-            console.log(`Error: ${error}`);
-        }
-    };
+	return (
+		<div>
+			<button onClick={handleFetch}>Fetch Pokemon</button>
+			{/* Render the fetched data here */}
+		</div>
+	);
 }
