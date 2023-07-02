e983e1123ef808bb5f5f929fe1ec571df2cbbd65
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-02 20:13:10 +0200

    Passing the props from the FetchData to the Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,31 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Button from './components/Button';
-import FetchPokemon from './Utils/FetchPokemon';
+
 import Card from './components/Card';
-import { fetchPoke, Pokemon } from './Utils/FetchPokemon';
+import FetchPokemon, { Pokemon } from './Utils/FetchPokemon';
 
 const Home = () => {
-	//   return (
-	//     <div>
-	//       <div className="flex flex-col gap-5 text-center w-max mx-auto my-4 h-24 border-slate-950 border px-12">
-	//         <h1>Pokemon</h1>
-	//         <Button onClick={FetchPokemon} label='Fetch Poke'/>
-	//       </div>
-	//       <div className="flex flex-col gap-5 text-center w-max mx-auto my-4 h-36 border-slate-950 border px-12">
-	//         {fetchedPoke && <span>{fetchedPoke.name}</span>}
-	//         {fetchedPoke && <Image src={fetchedPoke.image} alt='pokeImg' width={100} height={100}/>}
-	//         {/* {fetchedPoke && <span>{fetchedPoke.id}</span>} */}
-	//       </div>
-	//     </div>
-	//   );
+	const [fetchedPoke, setFetchedPoke] = useState<Pokemon | null>(null);
+
+	const handleFetchedPoke = (pokemon: Pokemon | null) => {
+		setFetchedPoke(pokemon);
+	};
+
 	return (
 		<div className='mt-4 flex items-center justify-center text-center'>
-			<Card />
-			<Button onClick={fetchPoke} label={'Fetch me'} />
+			<Card
+				pokeName={fetchedPoke?.name}
+				pokeID={fetchedPoke?.id}
+				pokeImg={fetchedPoke?.image}
+			/>
+			<div>
+				<FetchPokemon onFetchedPoke={handleFetchedPoke} />
+				{/* Render the fetchedPoke data from the state here */}
+				{fetchedPoke && <></>}
+			</div>
 		</div>
 	);
 };
